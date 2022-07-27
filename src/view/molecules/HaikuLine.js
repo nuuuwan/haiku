2f23eae11a3f0f7b1e380005b11adb952dd705d2
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-27 10:35:57 +0530

    Updated font size

--- src/view/molecules/HaikuLine.js
+++ src/view/molecules/HaikuLine.js
@@ -15,20 +15,21 @@ function getLineMetaData(line) {
   let lineStyleName = "p";
   if (line) {
     if (line.substring(0, 2) === "# ") {
-      variant = "h3";
+      variant = "h4";
       lineStr = line.substring(2);
       color = "#ccc";
       lineStyleName = "h1";
     } else if (line.substring(0, 3) === "## ") {
-      variant = "h4";
+      variant = "h5";
       lineStr = line.substring(3);
       color = "#888";
       lineStyleName = "h2";
     } else if (line.substring(0, 4) === "### ") {
-      variant = "h5";
+      variant = "h6";
       lineStr = line.substring(4);
       color = "#444";
       lineStyleName = "h3";
+
     } else if (line.substring(0, 2) === "* ") {
       variant = "li";
       lineStr = line.substring(2);
@@ -57,11 +58,16 @@ export default function HaikuLine({ line, isLastLine }) {
     innerText = <li>{innerText}</li>;
   }
 
+  let lineStyleNameLabel = "";
+  if (lineStyleName !== "p") {
+    lineStyleNameLabel = lineStyleName;
+  }
+
   return (
     <tr>
       <td style={STYLE_METADATA}>
         <Typography variant="caption" color="lightgray">
-          {lineStyleName}
+          {lineStyleNameLabel}
         </Typography>
       </td>
       <td>{innerText}</td>
